306d8be79f31c31dfc7a637a32e8dc7341034256
trangkhuc <khuctrang1812@gmail.com>
2019-11-16 20:57:24 +0700

    Updates

---
+++ precache-manifest.f0cb9644e1cddc3da6f6cfa4187e2b10.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "141eed107b01b6dc121d2f1880c1efa6",
+    "url": "/trello-clone/index.html"
+  },
+  {
+    "revision": "54b6d2ad7409ae758c55",
+    "url": "/trello-clone/static/js/2.bd5e185f.chunk.js"
+  },
+  {
+    "revision": "c01b5313aa9f940c775e",
+    "url": "/trello-clone/static/js/main.767892da.chunk.js"
+  },
+  {
+    "revision": "c8409f23391c14b691bb",
+    "url": "/trello-clone/static/js/runtime-main.aaae5460.js"
+  },
+  {
+    "revision": "03811d3289eabee453908984de979703",
+    "url": "/trello-clone/static/media/scene1.03811d32.jpg"
+  },
+  {
+    "revision": "1d01566a593e4844b8bba2ee6c0d40d1",
+    "url": "/trello-clone/static/media/scene2.1d01566a.jpg"
+  },
+  {
+    "revision": "4be965307d8a375cdd549d811210aadb",
+    "url": "/trello-clone/static/media/scene3.4be96530.jpg"
+  },
+  {
+    "revision": "c3461ca4c84f181441790db91bd83e88",
+    "url": "/trello-clone/static/media/scene4.c3461ca4.jpg"
+  },
+  {
+    "revision": "133f90dd6fa6d220ce149c84c5024c77",
+    "url": "/trello-clone/static/media/scene5.133f90dd.jpg"
+  },
+  {
+    "revision": "cf2b528cad7d7973061511cfeddb9002",
+    "url": "/trello-clone/static/media/scene6.cf2b528c.jpg"
+  },
+  {
+    "revision": "a33cadf50331f30f274d7a3a3994f103",
+    "url": "/trello-clone/static/media/scene7.a33cadf5.jpg"
+  },
+  {
+    "revision": "c17ddaef376bb11c8bf1369bf37b939a",
+    "url": "/trello-clone/static/media/scene8.c17ddaef.jpg"
+  },
+  {
+    "revision": "caa6d43a72f23664ce46dec9b62286fc",
+    "url": "/trello-clone/static/media/scene9.caa6d43a.jpg"
+  },
+  {
+    "revision": "de0fc6d326555b7c1af42c61cbb9191a",
+    "url": "/trello-clone/static/media/sunset.de0fc6d3.jpg"
+  }
+]);
